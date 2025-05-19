3bfebf9fa8ca6d0ff7058daca42afc35450e6005
yunipermatasari12345 <160714168+yunipermatasari12345@users.noreply.github.com>
2025-05-19 16:26:46 +0700

    compact View

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function homepage()
+    {
+        $movies = Movie::latest()->paginate(6);
+        return view('homepage', compact('movies'));
+    }
 }
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.template')
+
+
+@section('content')
+<h1>Latest Movies</h1>
+<div class="row">
+    @foreach ($movies as $movie )
+    <div class="col-lg-6">
+        <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="{{$movie ->cover_image }}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$movie->title}}</h5>
+                        <p class="card-text">{{Str::words($movie->synopsis,20,end:'...')}}</p>
+                        <a href="" class="btn btn-success">See More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
+    {{ $movies->links()}}
+</div>
+
+@endsection
